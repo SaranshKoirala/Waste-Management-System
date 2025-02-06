a9e7d37c6f51c85cb314e2c334f307629da53076
Saransh <upwhats498@gmail.com>
2025-02-06 21:03:33 +0545

    modified hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,11 @@
 import Navbar from "./Navbar";
-import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
 
 function Hero() {
-  useEffect(() => {
-    AOS.init({ duration: 1000, easing: "ease-in-out" });
-  }, []);
   return (
     <div className="relative bg-[url(/bg-image.jpg)] bg-cover bg-center h-screen mb-30">
       <Navbar />
-      <h1
-        data-aos="fade-left"
-        className="absolute poppins-font font-extrabold left-30 top-70 text-white text-7xl  w-[550px] text-center leading-18 align-middle"
-      >
+      <h1 className="absolute poppins-font font-extrabold left-30 top-70 text-white text-7xl  w-[550px] text-center leading-18 align-middle">
         WASTE MANAGEMENT SYSTEM
       </h1>
       <button className="absolute left-75 top-130 bg-white text-green-600 rounded-2xl py-2 px-5 poppins-font font-semibold text-center hover:scale-120 transition-all ease-in-out cursor-pointer">
