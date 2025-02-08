330a1dbaf1a31cc46254072effad65507a329594
Saransh <upwhats498@gmail.com>
2025-02-08 13:47:07 +0545

    added the form validation for the schedule pickup

--- src/pages/SchedulePickup.jsx
+++ src/pages/SchedulePickup.jsx
@@ -1,13 +1,64 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function SchedulePickup() {
+  const [fullName, setFullName] = useState("Saransh Koirala");
+  const [email, setEmail] = useState("upwhats498@gmail.com");
+  const [contact, setContact] = useState("");
+  const [streetName, setStreetName] = useState("");
+  const [wardNumber, setWardNumber] = useState(0);
+  const [pickupDate, setPickupDate] = useState("");
+  const [pickupTime, setPickupTime] = useState("Morning");
+  const [wasteType, setWasteType] = useState("Biodegradable");
+
+  function handleContactNumber(e) {
+    const value = e.target.value.replace(/\D/g, ""); // Remove non-numeric characters
+    setContact(value);
+  }
+
+  function handleCancelBtn(e) {
+    e.preventDefault();
+    setContact("");
+    setStreetName("");
+    setWardNumber(0);
+    setPickupDate("");
+    setPickupTime("Morning");
+    setWasteType("Biodegradable");
+  }
+
+  function handleSubmitBtn(e) {
+    e.preventDefault();
+
+    if (contact.length > 10) {
+      alert("Invalid contact number!");
+      return;
+    } else if (wardNumber > 35 || wardNumber === 0) {
+      alert("Invalid ward number!");
+      return;
+    } else {
+      alert("Form submitted sucessfully!");
+    }
+
+    const formData = {
+      fullName,
+      email,
+      contact,
+      streetName,
+      wardNumber,
+      pickupDate,
+      pickupTime,
+      wasteType,
+    };
+    handleCancelBtn(e);
+  }
+
   return (
     <div className="bg-[url(/gradient-1.jpg)] bg-cover bg-center min-h-screen max-w-screen p-5">
       <Link to={"/"}>
         <img src="logo2.png" width={"70px"} />
       </Link>
       <div className="flex justify-end items-center -m-25 -mr-3">
-        <div className="poppins-font  -mr-5 text-green-950">
+        <div className="poppins-font -mr-5 text-green-950">
           <p className="font-light text-3xl text-end ">Let’s Tidy Up,</p>
           <p className=" text-4xl font-semibold">WASTE TRUCK IS READY!</p>
         </div>
@@ -21,36 +72,77 @@ function SchedulePickup() {
             your waste.
           </p>
         </div>
-        <form className="grid grid-cols-3 justify-start items-center gap-10">
+        <form
+          className="grid grid-cols-3 justify-start items-center gap-10"
+          onSubmit={handleSubmitBtn}
+        >
           <div className="flex flex-col gap-2 w-96">
             <label>Full Name</label>
-            <input type="text" className="border rounded-sm p-1" disabled />
+            <input
+              type="text"
+              value={fullName}
+              className="border rounded-sm p-1"
+              disabled
+              required
+            />
           </div>
           <div className="flex flex-col gap-2 w-96">
             <label>Email</label>
-            <input type="email" className="border rounded-sm p-1" disabled />
+            <input
+              type="email"
+              value={email}
+              className="border rounded-sm p-1"
+              disabled
+              required
+            />
           </div>
           <div className="flex flex-col gap-2 w-96">
             <label>Contact Number</label>
-            <input type="number" className="border rounded-sm p-1" />
+            <input
+              className="border rounded-sm p-1"
+              type="tel"
+              value={contact}
+              onChange={handleContactNumber}
+              required
+            />
           </div>
           <div className="flex flex-col gap-2 w-96">
             <label>Street Name</label>
-            <input type="text" className="border rounded-sm p-1" />
+            <input
+              className="border rounded-sm p-1"
+              type="text"
+              value={streetName}
+              onChange={(e) => setStreetName(e.target.value)}
+              required
+            />
           </div>
           <div className="flex flex-col gap-2 w-96">
             <label>Ward Number</label>
-            <input type="number" className="border rounded-sm p-1" />
+            <input
+              className="border rounded-sm p-1"
+              type="number"
+              value={wardNumber}
+              onChange={(e) => Number(setWardNumber(e.target.value))}
+              required
+            />
           </div>
           <div className="flex flex-col gap-2 w-96">
             <label>Pickup Date</label>
-            <input type="date" className="border rounded-sm p-1" />
+            <input
+              className="border rounded-sm p-1"
+              type="date"
+              value={pickupDate}
+              onChange={(e) => setPickupDate(e.target.value)}
+              required
+            />
           </div>
           <div className="flex flex-col gap-2 w-96 overflow-visible">
             <label>Pickup Time</label>
             <select
-              defaultValue={"Morning"}
               className="block border rounded-sm p-1"
+              value={pickupTime}
+              onChange={(e) => setPickupTime(e.target.value)}
+              required
             >
               <option value={"Morning"}>Morning🌅</option>
               <option value={"Afternoon"}>Afternoon🌞</option>
@@ -60,24 +152,27 @@ function SchedulePickup() {
           <div className="flex flex-col gap-2 w-96 overflow-visible">
             <label>Waste Type</label>
             <select
-              defaultValue={"Biodegradable Waste"}
               className="block border rounded-sm p-1"
+              value={wasteType}
+              onChange={(e) => setWasteType(e.target.value)}
+              required
             >
-              <option value={"Biodegradable Waste"}>
-                Biodegradable Waste♻️
-              </option>
-              <option value={"Non-Biodegradable Waste"}>
+              <option value={"Biodegradable"}>Biodegradable Waste♻️</option>
+              <option value={"Non-Biodegradable"}>
                 Non-Biodegradable Waste🚯
               </option>
             </select>
           </div>
           <div className="flex justify-center items-center gap-20 w-96 -mb-6">
-            <button className="w-30 cursor-pointer bg-gray-600 hover:bg-gray-400 text-white font-semibold py-2 px-4 rounded-lg shadow-md transition-all">
+            <button
+              className="w-30 cursor-pointer bg-gray-600 hover:bg-gray-400 text-white font-semibold py-2 px-4 rounded-lg shadow-md transition-all"
+              onClick={handleCancelBtn}
+            >
               Cancel
             </button>
 
             <button className="w-30 cursor-pointer bg-green-600 hover:bg-green-400 text-white font-semibold py-2 px-4 rounded-lg shadow-md transition-all">
-              Submmit
+              Submit
             </button>
           </div>
         </form>
