bf10993d3a3ee8366a651b76e64fec65d7cc979c
Saransh <upwhats498@gmail.com>
2025-02-11 07:16:50 +0545

    updated the user route

--- backend/Routes/userRoute.js
+++ backend/Routes/userRoute.js
@@ -30,6 +30,35 @@ route.get("/:id", async (req, res) => {
   }
 });
 
-route.post("/");
+route.delete("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(400).json({ message: "User Not Found!" });
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "User deleted sucessfully!" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+route.put("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(400).json({ message: "User Not Found!" });
+
+    const { name, email, password } = req.body;
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { name, email, password },
+      { new: true, runValidators: true }
+    );
+
+    res
+      .status(200)
+      .json({ message: "Updated Succesfully!", data: updatedUser });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 export default route;
